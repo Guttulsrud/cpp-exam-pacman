48d4a002bf6ed67a3627beb633a958d67208a4ad
mathias <mathias_a_sandnes@hotmail.com>
2020-03-30 18:25:10 +0200

    Merge branch 'feature-InputManagement' of C:\Users\mathi\CLionProjects\cpp-home-exam with conflicts.

---
+++ src/Player.cpp
@@ -0,0 +1,77 @@
+//
+// Created by mathiss on 3/25/2020.
+//
+
+#include "../include/Player.h"
+#include "../include/InputManager.h"
+#include "../include/Game.h"
+#include <typeinfo>
+#include <iostream>
+
+void Player::update() {
+
+    SDL_Rect newPosition = m_positionRectangle;
+    bool changedDirection = false;
+    DIRECTION oldDirection = direction;
+
+    if (InputManager::getInstance().KeyStillDown(SDL_SCANCODE_W)) {
+        direction = UP;
+        changedDirection = true;
+
+    } else if (InputManager::getInstance().KeyStillDown(SDL_SCANCODE_A)) {
+        direction = LEFT;
+        changedDirection = true;
+
+    } else if (InputManager::getInstance().KeyStillDown(SDL_SCANCODE_S)) {
+        direction = DOWN;
+        changedDirection = true;
+
+    } else if (InputManager::getInstance().KeyStillDown(SDL_SCANCODE_D)) {
+        direction = RIGHT;
+        changedDirection = true;
+    }
+
+    switch (direction) {
+        case UP:
+            newPosition.y -= m_movementSpeed;
+            break;
+        case DOWN:
+            newPosition.y += m_movementSpeed;
+            break;
+        case RIGHT:
+            newPosition.x += m_movementSpeed;
+            break;
+        case LEFT:
+            newPosition.x -= m_movementSpeed;
+            break;
+        default:
+            break;
+    }
+
+    /// Collision
+    bool collidedWithWall = false;
+    std::for_each(
+            std::begin(Game::getGameObjects()), std::end(Game::getGameObjects()),
+            [this, &collidedWithWall, newPosition](std::shared_ptr<GameObject> &object) {
+                if (m_id != object->m_id) {
+                    if (SDL_HasIntersection(&newPosition, &object->m_positionRectangle) && object->getType() == "Wall") {
+                        collidedWithWall = true;
+                    }
+                }
+            }
+    );
+
+
+    if (!collidedWithWall) {
+        m_positionRectangle = newPosition;
+    } else if(changedDirection){
+        direction = oldDirection;
+    }
+    //////
+
+}
+
+std::string Player::getType() {
+    return "Player";
+}
+
