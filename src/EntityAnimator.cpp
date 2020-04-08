09fb965f2dd8bc8e2a6b6c28afccdfb65ab8c1b5
mathias <mathias_a_sandnes@hotmail.com>
2020-04-08 15:43:23 +0200

    Added player ghost animation

--- src/EntityAnimator.cpp
+++ src/EntityAnimator.cpp
@@ -5,7 +5,7 @@
 #include "../include/EntityAnimator.h"
 #include "../include/Game.h"
 
-void EntityAnimator::animate(SDL_Texture ** entityTexturePtr, Direction direction) {
+void EntityAnimator::animate(SDL_Texture **entityTexturePtr, Direction direction) {
 
     if (direction == NONE) {
         Game::getPlayer()->m_texture = textures[UP][0];
@@ -15,7 +15,7 @@ void EntityAnimator::animate(SDL_Texture ** entityTexturePtr, Direction directio
         animationIndex++;
         frameCount = 0;
     }
-    if(animationIndex == textures[direction].size()){
+    if (animationIndex == textures[direction].size()) {
         animationIndex = 0;
     }
 
