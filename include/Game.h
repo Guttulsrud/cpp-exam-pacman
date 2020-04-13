5b74e5dd40b678d58d361673b35ac9e023f022ad
martin.hallan@gmail.com <martin.hallan@gmail.com>
2020-04-13 23:57:53 +0200

    fixed

--- include/Game.h
+++ include/Game.h
@@ -45,7 +45,7 @@ public:
     static void addMovableGameObject(const std::shared_ptr<MovableObject> &object);
     static void addStationaryGameObject(const std::shared_ptr<StationaryObject> &object);
     void setMap(int map);
-    int activeMap = 3;
+    int activeMap = 2;
     std::map<int, std::shared_ptr<Map>> maps;
     void resetRound();
     void startGame();
